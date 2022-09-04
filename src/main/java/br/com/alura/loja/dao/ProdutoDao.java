19c9a716e50f8a177caded7f8c82f38f99161001
Francisco de Assis <a124fr@gmail.com>
2022-09-04 00:00:27 -0300

    Trabanlhdo com  consultas feitas em jpql

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,8 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -25,4 +28,43 @@ public class ProdutoDao {
 		this.em.remove(produto);
 	}
 	
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class)
+				.getResultList();
+	}
+	
+	public List<Produto> buscarPorNome(String nome) {
+		// usando parametro nomeado (named param)
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nome", nome)
+				.getResultList();
+	}
+	
+	public List<Produto> buscarPorNome2(String nome) {
+		// usando parametro posicional
+		String jpql = "SELECT p FROM Produto p WHERE p.nome = ?1";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter(1, nome)
+				.getResultList();
+	}
+	
+	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nomeDaCategoria";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter("nomeDaCategoria", nome)
+				.getResultList();
+	}
+	
+	public BigDecimal buscarPrecoDoProdutoComNome(String nome) {
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, BigDecimal.class)
+				.setParameter("nome", nome)
+				.getSingleResult();
+	}
 }
--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -90,4 +90,10 @@ public class Produto {
 		this.categoria = categoria;
 	}
 
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco
+				+ ", dataCadastro=" + dataCadastro + ", categoria=" + categoria + "]";
+	}
+
 }
